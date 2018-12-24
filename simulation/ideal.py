0f11b0b81aacdfba6588e47db54ea8e532aed30d
Dionysis Zindros <dionyziz@gmail.com>
2018-12-24 16:37:30 +0100

    Add ideal egalitarian curve figure

---
+++ simulation/ideal.py
@@ -0,0 +1,40 @@
+from matplotlib import rc
+import matplotlib.pyplot as plt
+import numpy as np
+from matplotlib.backends.backend_pdf import PdfPages
+
+
+plt.rcParams['text.latex.preamble'] = [r"\usepackage{lmodern}"]
+
+pp = PdfPages('../figures/ideal.pdf')
+
+rc('text', usetex=True, **{'latex.unicode': True})
+rc(
+    'font',
+    family='serif',
+    serif=['Computer Modern Roman'],
+    monospace=['Computer Modern Typewriter']
+)
+
+MAX_CAPITAL = 1000000
+
+x = np.linspace(0, MAX_CAPITAL, MAX_CAPITAL)
+y = np.zeros(MAX_CAPITAL) + 0.07
+
+fig = plt.figure()
+fig.set_size_inches(6.2, 6.2)
+
+plt.ylim(0, 0.2)
+plt.plot(x, y, label='Ideal egalitarian curve')
+
+plt.xlabel('Investment Capital (nominal 2018 USD)')
+plt.ylabel('Freshly generated ROI')
+
+plt.title('Ideal egalitarian curve')
+
+plt.legend()
+plt.gca().set_yticklabels(['{:.0f}\%'.format(x*100) for x in plt.gca().get_yticks()])
+
+plt.savefig(pp, format='pdf', dpi=1000, bbox_inches='tight')
+
+pp.close()
